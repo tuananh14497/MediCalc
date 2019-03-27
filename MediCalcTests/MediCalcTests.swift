0a2023ac791cafa75e1d41fbbf77e97a6e07f010
Tuấn Anh Ngô <tuananh14497@gmail.com>
2019-03-27 17:20:24 +0700

    Initial Commit

---
+++ MediCalcTests/MediCalcTests.swift
@@ -0,0 +1,34 @@
+//
+//  MediCalcTests.swift
+//  MediCalcTests
+//
+//  Created by Anh Ngo on 3/27/19.
+//  Copyright © 2019 Anh Ngo. All rights reserved.
+//
+
+import XCTest
+@testable import MediCalc
+
+class MediCalcTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
