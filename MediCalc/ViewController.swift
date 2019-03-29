e0e711d62436c3731314f4c107158f3ae201625a
Tuấn Anh Ngô <tuananh14497@gmail.com>
2019-03-29 15:00:34 +0700

    First Commit

--- MediCalc/ViewController.swift
+++ MediCalc/ViewController.swift
@@ -8,13 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+
+    @IBOutlet weak var hamLuongTextField: UITextField!
+    @IBOutlet weak var mlTextField: UITextField!
+    @IBOutlet weak var hamLuongCanTextfield: UITextField!
+    @IBOutlet weak var resultLable: UILabel!
+    @IBOutlet weak var calculateBtn: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        calculateBtn.isUserInteractionEnabled = false
+//        calculateBtn.alpha = 0.5
+        
     }
-
-
+    
+    
+    @IBAction func docalculation(_ sender: Any) {
+       
+        if allFilled() {
+            let hamluong = Float(hamLuongTextField.text!)
+            let ml = Float(mlTextField.text!)
+            let hlCan = Float(hamLuongCanTextfield.text!)
+            
+            let num = Float((hlCan! * ml!)/hamluong!)
+            let resultString = String(num)
+            
+            resultLable.text = resultString
+        } else {
+            resultLable.text = "K. để trống"
+        }
+    
+    }
+    
+    
+    func allFilled() -> Bool {
+        let hamLuong = hamLuongTextField.text
+        let ml = mlTextField.text
+        let hamLuongCan = hamLuongCanTextfield.text
+        
+        if (hamLuong != "" && ml != "" && hamLuongCan != "") {
+            return true
+        } else { return false }
+    }
 }
+
+
+
+
 
